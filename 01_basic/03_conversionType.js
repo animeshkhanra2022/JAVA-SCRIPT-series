f038327c3639a4c1813647ffc5c508a293b7ffe8
Animesh <134761066+animeshkhanra2022@users.noreply.github.com>
2024-07-14 09:08:03 +0530

    conversion of dataType

---
+++ 01_basic/03_conversionType.js
@@ -0,0 +1,50 @@
+// *********************Conversion of data Type **************************
+//             number to string / boolean / number conversion 
+
+let score = 0
+console.log(typeof score);
+
+let convScore = Number(score)
+console.log(typeof convScore);
+console.log(convScore);
+
+// convert to string **************************
+
+let convStr = String(score)
+console.log(typeof convStr);
+console.log(convStr);
+
+// convert to BOOLEAN **************************
+
+let convBo = Boolean(score)
+console.log(typeof convBo);
+console.log(convBo);
+
+//  NOTE 
+// IF NUMBER IS 1 OR MORE 1 VALUE => TURE
+// IF NUMBER IS 0  => FALSE
+
+// -------------------------String to string / boolean / number conversion-------------------
+
+let a = "33asd"
+console.log(typeof a)
+
+// convert to number .........................
+let convA = Number(a)
+console.log(typeof convA);
+console.log(convA);
+
+// note *********
+// "33asd" => NaN (Not a Number)
+
+// convert to boolean .........................
+
+let convBool = Boolean(a)
+console.log(typeof convBool);
+console.log(convBool);
+
+//  NOTE ....................
+// IF NUMBER IS 1 OR MORE 1 VALUE or any STRING VALUE => TURE
+// IF NUMBER IS 0  => FALSE
+
+console.table([convStr,convBo,convScore,convA,convBool])
