d5f534e2870cacc6380429d6b39523a83c4d6d44
Animesh <134761066+animeshkhanra2022@users.noreply.github.com>
2024-07-15 12:15:42 +0530

    add 05_string file

---
+++ 01_basic/04_strings.js
@@ -0,0 +1,34 @@
+const name = "Animesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+
+const numb = 23
+console.log(numb);
+
+let numValue = new String('Animesh')     // This is an object 
+// console.log(numValue);
+console.log(numValue[0]);
+console.log(numValue[1]);
+console.log(numValue[2]);
+console.log(numValue[3]);
+console.log(numValue[4]);
+console.log(numValue[5]);
+console.log(numValue[6]);
+
+console.log(numValue.toUpperCase());  //^ to change all letter on upper case
+let uppCase = numValue.toUpperCase()
+
+console.log(uppCase.toLowerCase());   //^ to change all letter on lower case
+
+console.log(numValue.charAt(4))       //^  to see in 4th position which letter have
+console.log(numValue.indexOf('e'))       //^  to see the letter 'e' in which position
+
+let gameName = new String("Aniemsh khanra is a man")
+
+console.log(gameName.split(' '))
+
