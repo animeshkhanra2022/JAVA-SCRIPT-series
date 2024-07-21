68f8f512dcb733afbc34bf13b961d7e04e9052e3
Animesh <134761066+animeshkhanra2022@users.noreply.github.com>
2024-07-21 20:23:42 +0530

    Add new 3 file

---
+++ 04_controlFlow.js/01_ifControl.js
@@ -0,0 +1,96 @@
+// **************   IF  ******************
+// let temp = 41;
+
+// if (temp > 44) {
+//     console.log("The temparatuer is high.");
+// }
+
+//** ***********************  if-else ************************* */
+
+// if (temp > 44) {
+//     console.log("The temparatuer is high.");
+// }else{
+//     console.log("Tenparature is normal.")
+// }
+
+//***************************  IF-ELSE-ELSE  ************************ */
+
+// let temp = 27;
+
+// if (temp == 27) {
+//     console.log("Tenparature is normal.");
+// }else if(temp > 27){
+//     console.log("The temparatuer is high.");
+// }else if(temp < 27){
+//     console.log("Temparature is low");
+// }else{
+//     console.log("You are not define.");
+// }
+
+// ******************** NESTED IF ELSE ****************************
+// let temp = 610;
+
+// if (temp < 27) {
+//     console.log("Tenparature is LOW");
+//     if (temp > 0){
+//         console.log("Water is cool but not freezed");
+//     }else{
+//         console.log("Water is freezed");
+//     }
+// }else if(temp > 27){
+//     console.log("The temparatuer is high.");
+// }
+
+
+//******************************  for loop  ********************* */
+
+// let str = '';
+
+// for (let i = 0; i < 9; i++) {
+//   str = str + i;
+// }
+
+// console.log(str);
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course");
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in");
+// }
