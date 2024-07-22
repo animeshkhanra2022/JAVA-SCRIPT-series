073de3a4771344e100f9ae019c25eb35a59c7d1d
Animesh <134761066+animeshkhanra2022@users.noreply.github.com>
2024-07-22 20:29:26 +0530

    add this file

---
+++ 05_iteration/02_whileloop.js
@@ -0,0 +1,33 @@
+//*****************  while loops  ************************ */
+
+// let n = 0;
+
+// while (n < 3) {
+//   n++;
+// }
+
+// console.log(n);
+//~ Expected output: 3
+
+// let index = 0;
+
+// while (index < 5) {
+//     console.log(`Number is : ${index}`);
+//     index+=1
+// }
+
+
+// let myArray = ['flash', "batman", "superman"]
+
+// let arr = 0
+// while(arr < myArray.length){
+//     console.log(`My superhero is : ${myArray[arr]}`);
+//     arr++
+// }
+
+let score = 0
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
