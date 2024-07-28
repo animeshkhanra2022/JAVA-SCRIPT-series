34cfc073269e77505de50460daab137dc3d5fe9d
Animesh <134761066+animeshkhanra2022@users.noreply.github.com>
2024-07-28 09:17:52 +0530

    Add file with Projects.

---
+++ 07_Projects/02_project/chaiaurcode.js
@@ -0,0 +1,54 @@
+const form = document.querySelector('form');
+
+
+form.addEventListener('submit', function(e){
+  e.preventDefault()
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = (document.querySelector('#results'));
+  const results1 = (document.querySelector('#results1'));
+
+  if(height === '' || height <= 0 || isNaN(height)){
+    results.innerHTML = `Please give me a valid height ${height}`
+  }else if(weight === '' || weight <= 0 || isNaN(weight)){
+    results.innerHTML = `Please give me a valid weight ${weight}`
+  }else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    // console.log(bmi)
+    results.innerHTML = `<span>${bmi}</span>`;
+
+    if (bmi < 18.6) {
+      results1.innerText = "You are Under Weight"
+    }else if(bmi > 24.9 ) {
+      results1.innerText = "You are Over Weight"
+    }else{
+      results1.innerText = "You are Normal"
+    }
+  }
+
+
+
+})
+
+// const form = document.querySelector('form');
+// // this usecase will give you empty
+// // const height = parseInt(document.querySelector('#height').value)
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+
+//   const height = parseInt(document.querySelector('#height').value);
+//   const weight = parseInt(document.querySelector('#weight').value);
+//   const results = document.querySelector('#results');
+
+//   if (height === '' || height < 0 || isNaN(height)) {
+//     results.innerHTML = `Please give a valid height ${height}`;
+//   } else if (weight === '' || weight < 0 || isNaN(weight)) {
+//     results.innerHTML = `Please give a valid weight ${weight}`;
+//   } else {
+//     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+//     //show the result
+//     results.innerHTML = `<span>${bmi}</span>`;
+//   }
+// });
